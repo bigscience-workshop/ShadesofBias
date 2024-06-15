2a6f671b74e2e146f173e78465c5913d875dfe2a
jaddoughman <jad.doughman@outlook.com>
2024-06-15 21:42:37 +0300

    generating bias boxplot grouped by both language and model

--- logprob_difference.py
+++ logprob_difference.py
@@ -70,7 +70,7 @@ def calculate_logprob_difference(subset_df, langs, models):
 def process_data(df):
     config = Config()
     langs = ['en', 'fr', 'zh'] #config.language_codes.values() #['en', 'es', 'ru', 'bn', 'zh', 'nl', 'fr', 'de', 'hi', 'it', 'mr', 'pl', 'ro', 'ru']
-    models = ['bigscience_bloom-7b1']
+    
     gendered_dfs = {}
     for gender_identity in (GROUP1_STR, GROUP2_STR):
         bias_df = pd.DataFrame()
@@ -121,7 +121,21 @@ def generate_boxplot(bias_df, identity=None):
     fig.show()
 
 if __name__ == "__main__":
-    df = pd.DataFrame(load_dataset("LanguageShades/BiasShadesBaseEval_bigscience_bloom-7b1")['test']).dropna(subset=['index'])
-    bias_dict = process_data(df)
-    generate_boxplot(bias_dict[GROUP1_STR], GROUP1_STR)
-    generate_boxplot(bias_dict[GROUP2_STR], GROUP2_STR)
+    models = ['bigscience_bloom-7b1','bigscience_bloom-7b1']
+
+    bias_all_models_g1 = pd.DataFrame()
+    bias_all_models_g2 = pd.DataFrame()
+
+    for model_name in models:
+        df = pd.DataFrame(load_dataset(f"LanguageShades/BiasShadesBaseEval_{model_name}")['test']).dropna(subset=['index'])
+        bias_dict = process_data(df)
+
+        bias_all_models_g1 = pd.concat([bias_all_models_g1, bias_dict[GROUP1_STR]])
+        bias_all_models_g2 = pd.concat([bias_all_models_g2, bias_dict[GROUP2_STR]])
+
+        ######### TO DELETE #########
+        bias_all_models_g1.loc[bias_all_models_g1.tail(20).index, 'model'] = 'test_model'
+        ##############################
+    
+    generate_boxplot(bias_all_models_g1, GROUP1_STR)
+    generate_boxplot(bias_all_models_g2, GROUP2_STR)
