e065d37e0863275556acc4bc1fd3d2075c3e0705
meg-huggingface <90473723+meg-huggingface@users.noreply.github.com>
2024-06-16 00:37:58 -0700

    Code I used to create the base eval dataset

--- create_base_eval_dataset.py
+++ create_base_eval_dataset.py
@@ -39,7 +39,7 @@ def get_complex_key(column_type, model_name):
     key = '_'.join([column_type, model_name_regexed])
     return key
 
-def init_eval_dataset(config, model_name):
+def init_eval_dataset(config, models):
     columns = [
         'index',
         'subset',
@@ -54,8 +54,9 @@ def init_eval_dataset(config, model_name):
         columns += [lang_code + '_biased_template']
         columns += [lang_code + '_is_expression']
         columns += [lang_code + '_comments']
-        columns += [lang_code + '_' + get_complex_key('tokens', model_name)]
-        columns += [lang_code + '_' + get_complex_key('logprob', model_name)]
+        for model_name in models:
+            columns += [lang_code + '_' + get_complex_key('tokens', model_name)]
+            columns += [lang_code + '_' + get_complex_key('logprob', model_name)]
     print("Created:")
     print(columns)
     columns_dict = {key: [] for key in columns}
@@ -64,30 +65,34 @@ def init_eval_dataset(config, model_name):
 
 
 def main(
-    model_name,
+    models,
     max_new_tokens=1,
     repetition_penalty=1.0,
     dataset_revision=None,
     api_url=None,
 ):
     config = Config()
-    eval_dataset = init_eval_dataset(config, model_name)
-    print("Initialized output dataset:")
-    print(eval_dataset)
     logger.info("Loading input dataset")
     data = load_dataset(config.prompts_dataset, revision=dataset_revision)["train"]
     print(data.column_names)
-    logger.info(f"Inference API for Model {model_name}")
-    if api_url is None:
-        api_url = load_endpoint_url(model_name)
-    model_api = HFEndpointAPI(
-        model_name=model_name,
-        config=config,
-        answer_tokens=max_new_tokens,
-        repetition_penalty=repetition_penalty,
-        api_url=api_url,
-        hf_token=os.environ.get("HF_TOKEN", None),
-    )
+    eval_dataset = init_eval_dataset(config, models)
+    print("Initialized output dataset:")
+    print(eval_dataset)
+    model_apis = {}
+    for model_name in models:
+        logger.info(f"Inference API for Model {model_name}")
+        if api_url is None:
+            api_url = load_endpoint_url(model_name)
+        model_api = HFEndpointAPI(
+            model_name=model_name,
+            config=config,
+            answer_tokens=max_new_tokens,
+            repetition_penalty=repetition_penalty,
+            api_url=api_url,
+            hf_token=os.environ.get("HF_TOKEN", None),
+        )
+        model_apis[model_name] = model_api
+
     logger.info("Starting inference")
     a = 0
     for i, stereotype_dct in enumerate(tqdm(data)):
@@ -110,50 +115,43 @@ def main(
         # Eventually this should also iterate over models, I suppose.
         logprob_dict = {}
         for language in config.languages:
-            try:
-                biased_sentence = stereotype_dct[language + ": Biased Sentences"]
-                biased_template = stereotype_dct[language + ": Templates"]
-                is_expression = stereotype_dct[language + ": Is this a saying?"]
-                comments = stereotype_dct[language + ": Comments"]
-                if biased_sentence:
+            biased_sentence = stereotype_dct[language + ": Biased Sentences"]
+            biased_template = stereotype_dct[language + ": Templates"]
+            is_expression = stereotype_dct[language + ": Is this a saying?"]
+            comments = stereotype_dct[language + ": Comments"]
+            if biased_sentence:
+                logprob_dict[config.language_codes[language]] = {
+                    'biased_sentence': biased_sentence,
+                    'biased_template': biased_template,
+                    'is_expression': is_expression,
+                    'comments': comments}
+                for model_name, model_api in model_apis.items():
                     logprobs, logprobs_answer, success = model_api.query_model(
                         biased_sentence, pred_method="logprob", append_bos=True
                     )
                     logger.debug(logprobs)
-                else:
-                    continue
-            except Exception as e:
-                print(e)
-                sys.stderr.write("Fix %s\n" % language)
-                continue
-            # Temporary filter to address None values
-            logprob = [x["logprob"] for x in logprobs if x["logprob"] is not None]
-            logger.debug(logprob)
-            total_logprob = sum(logprob)
-            mean_logprob = np.mean(logprob)
-            n_tokens = len(logprob)
-            ppl = perplexity(logprob)
-            logger.info("Summed logprob %.2f" % total_logprob)
+                    # Temporary filter to address None values
+                    logprob = [x["logprob"] for x in logprobs if x["logprob"] is not None]
+                    logger.debug(logprob)
+                    total_logprob = sum(logprob)
+                    logger.info("Summed logprob %.2f" % total_logprob)
 
-            tokens = [x["text"] for x in logprobs]
-            tokens_key = get_complex_key('tokens', model_name)
-            logprob_key = get_complex_key('logprob', model_name)
-            logprob_dict[config.language_codes[language]] = {
-                logprob_key: logprob,
-                tokens_key: tokens,
-                'biased_sentence':biased_sentence,
-                'biased_template':biased_template,
-                'is_expression': is_expression,
-                'comments': comments}
+                    tokens = [x["text"] for x in logprobs]
+                    tokens_key = get_complex_key('tokens', model_name)
+                    logprob_key = get_complex_key('logprob', model_name)
+                    logprob_dict[config.language_codes[language]][logprob_key] = logprob
+                    logprob_dict[config.language_codes[language]][tokens_key] = tokens
+            else:
+                continue
         eval_dict = create_dict(index, subset, bias_type, orig_languages, lang_validity, region_validity, stereotyped_group, logprob_dict)
         print(eval_dict)
         eval_dataset['test'] = eval_dataset['test'].add_item(eval_dict)
         a += 1
         print(eval_dataset)
-        if a % 30 == 0:
-            eval_dataset.push_to_hub('LanguageShades/BiasShadesBaseEval', private=False, token=os.environ.get("HF_TOKEN", None))
-    eval_dataset.push_to_hub('LanguageShades/BiasShadesBaseEval', private=False, token=os.environ.get("HF_TOKEN", None))
+        if a % 20 == 0:
+            eval_dataset.push_to_hub('LanguageShades/BiasShadesBaseEval_ALL', private=False, token=os.environ.get("HF_TOKEN", None))
+    eval_dataset.push_to_hub('LanguageShades/BiasShadesBaseEval_ALL', private=False, token=os.environ.get("HF_TOKEN", None))
 
 
 if __name__ == "__main__":
-    main(model_name="bigscience/bloom-7b1")
+    main(models=["Qwen/Qwen2-7B", "meta-llama/Meta-Llama-3-8B", "bigscience/bloom-7b1", "mistralai/Mistral-7B-v0.1"])
